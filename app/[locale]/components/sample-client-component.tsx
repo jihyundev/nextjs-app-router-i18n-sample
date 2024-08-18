45c296434e51af4b0d7cb910cc60d6ec71da6e60
jihyeon <jihyeon@makestar.com>
2024-08-18 15:54:07 +0900

    init i18n with next-intl

---
+++ app/[locale]/components/sample-client-component.tsx
@@ -0,0 +1,10 @@
+import {useTranslations} from 'next-intl';
+
+export const SampleClientComponent = () => {
+    const t = useTranslations('blog')
+    return (
+        <p className="mb-10 text-xl text-center">
+            {t('appDir.answer')}
+        </p>
+    )
+}
